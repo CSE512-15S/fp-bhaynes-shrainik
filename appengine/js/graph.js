92d3696f14ba79d935112255d4e55603e12f9094
Brandon Haynes <bhaynes@cs.washington.edu>
2015-06-07 12:37:59 -0700

    Removed redundant system name in fragment header

--- appengine/js/graph.js
+++ appengine/js/graph.js
@@ -674,7 +674,7 @@ function Graph () {
 
             node.select(".node-label text")
                 .text(function(d) {
-                    return d.name + ' (' + d.rawData.system + ')';
+                    return d.name;
                 })
                 .transition().duration(animationDuration)
                 .transition().duration(shortDuration)
